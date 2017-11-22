cf9b3942d9d7e2e6bd62bdd2e882210b7c76c93f
AlexisCaron <alexis.caron@viacesi.fr>
2017-11-22 10:20:17 +0100

    Add files via upload

--- Code_Arduino.ino
+++ Code_Arduino.ino
@@ -5,6 +5,9 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
+int peltier = 5;
+int power = 0;
+int peltier_level = map(power, 0, 99, 0, 255);
 float h = 0;
 float t = 0;
 float r = 10000.00;
@@ -15,16 +18,36 @@ void setup(){
 }
 
 void loop(){
+        
         h = dht.readHumidity();
         t = dht.readTemperature();
         double reader = analogRead(0);
         float temp1 = 0;
-
+        char option;
+        
         temp1 = (1023.00 / reader ) ;
         temp1 = (temp1 - 1);
         temp1 = (10000 / temp1);
-        
-                Serial.print("Humidite :");
+
+        if(Serial.available() > 0)
+  {
+     option = Serial.read();
+     if(option == 'a')
+     power += 5;
+     else if(option == 'z')
+     power -= 5;
+
+     if (power > 99) power = 99;
+     if (power < 0) power = 0;
+
+     peltier_level = map(power, 0, 99, 0, 255);
+  }
+                Serial.print("Power = ");
+                Serial.print(power);
+                Serial.print(" PLevel = ");
+                Serial.print(peltier_level);
+                analogWrite(peltier, peltier_level);
+                Serial.print("\n Humidite :");
                 Serial.print(h);
                 Serial.print(" %\t");
                 Serial.print("Temperature du frigo :");
@@ -33,6 +56,6 @@ void loop(){
                 Serial.print("\n Temperature exterieure : ");
                 Serial.println(temp1);
         
-        delay(2500);
+        delay(1000);
 }
 
